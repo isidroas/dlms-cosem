0f52ed437bc01942e03936597a5aa474c3b8e838
Isidro Arias <isidro.arias@wslw.es>
2024-02-06 13:11:19 +0100

    refactor dedicated ciphering

--- dlms_cosem/protocol/xdlms/general_ded_cipher.py
+++ dlms_cosem/protocol/xdlms/general_ded_cipher.py
@@ -1,5 +1,12 @@
+from dlms_cosem.protocol.xdlms.base import AbstractXDlmsApdu
 from dlms_cosem.protocol.xdlms.general_global_cipher import GeneralGlobalCipher
 
-class GeneralDedCipher(GeneralGlobalCipher):
+# TODO: move implementation from GeneralGlobalCipher to KeySpecificCiphering
+KeySpecificCipher = type(
+    "KeySpecificCipher", (AbstractXDlmsApdu,), dict(GeneralGlobalCipher.__dict__)
+)
+
+
+class GeneralDedCipher(KeySpecificCipher):
     TAG = 220
     NAME = "general-ded-cipher"
